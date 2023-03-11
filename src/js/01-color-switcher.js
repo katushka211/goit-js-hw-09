5e8487f36cd2871348278b18824a7a62ce2b5922
Kateryna Verveda <katushka211@gmail.com>
2023-03-11 16:56:50 +0100

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let switchInterval = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(e) {
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+  switchInterval = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick(e) {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(switchInterval);
+}
